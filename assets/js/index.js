45d89482753df26f46d5b0be18ec90f1359d8875
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2022-12-12 23:19:54 +0500

    added player to teasers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
 let menu = document.querySelector(".menu")
+let video = document.querySelector(".teasers__video")
+let play = document.querySelector(".teasers__start-btn")
+let playImg = document.querySelector(".teasers__play-img")
+let play2 = document.querySelector(".teasers__play-btn")
+let progress = document.querySelector(".teasers__video-progress")
+let time = document.querySelector(".video__current-time")
+let range = document.querySelector(".teasers__video-range")
 
 if (window.outerWidth < 900) {
     menu.addEventListener("click", () => {
@@ -13,4 +20,47 @@ if (window.outerWidth < 900) {
     })
 }
 
-document.querySelector("#range")
+play.addEventListener("click", playVideo)
+video.addEventListener("click", playVideo)
+play2.addEventListener("click", playVideo)
+
+function playVideo() {
+    if (video.paused) {
+        video.play()
+        play.style.display = "none"
+        playImg.src = "./assets/images/pause.png"
+    }else{
+        video.pause()
+        play2.style.display = "block"
+        play.style.display = "inline-block"
+        playImg.src = "./assets/images/play__teasers.svg"
+    }
+}
+
+video.addEventListener("timeupdate", progressVideo)
+
+function progressVideo() {
+    progress.style.width = `${(video.currentTime / video.duration) * 100}%`
+
+    let minutes = Math.floor(video.currentTime / 60)
+
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+
+    let seconds = Math.floor(video.currentTime % 60)
+
+    
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+
+    time.textContent = `${minutes}:${seconds}`
+}
+
+
+function changeRange() {
+    video.currentTime = (range.value * video.duration) / 100 
+}
+
+range.addEventListener("change" , changeRange)
